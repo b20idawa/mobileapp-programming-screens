fb1792ccc521a9b668bce1b9dd0e7ead200c53f7
b20idawa <b20idawa@student.his.se>
2021-04-19 11:16:50 +0200

    Skapat en Intent i onClick där knappen växlar från MainActivity.java till screen2.java. Därefter startar aktiviteten med intent.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, screen2.class);
+                startActivity(intent);
+                Log.d("knapp", "trycker på knappen");
             }
         });
     }
